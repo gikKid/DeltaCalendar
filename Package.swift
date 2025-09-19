2490c45ebcb542bcbb3575cc6030c52b3f7ed067
EN <en.saint@proton.me>
2025-09-19 14:27:04 +0300

    Remove tabs

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@ import PackageDescription
 
 let package = Package(
     name: "DeltaCalendar",
-	defaultLocalization: "en",
-	platforms: [.iOS(.v14)],
+    defaultLocalization: "en",
+    platforms: [.iOS(.v14)],
     products: [
-        .library( name: "DeltaCalendar", targets: ["DeltaCalendar"]),
+        .library(name: "DeltaCalendar", targets: ["DeltaCalendar"]),
     ],
-	dependencies: [
-		.package(url: "https://github.com/SnapKit/SnapKit", from: "5.7.1")
-	],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit", from: "5.7.1")
+    ],
     targets: [
         .target(name: "DeltaCalendar", dependencies: ["SnapKit"]),
         .testTarget(name: "DeltaCalendarTests", dependencies: ["DeltaCalendar"])
